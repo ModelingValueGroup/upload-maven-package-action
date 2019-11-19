b15ae6a978bfff26f2ff7f3298f46ab1f43a7f0e
Tom Brus <tombrus@gmail.com>
2019-11-19 19:38:41 +0100

    improve

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,78 +10,89 @@ export       gave="$1"; shift
 export  repoOwner="$1"; shift
 export   repoName="$1"; shift
 ###################################################
-
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-set
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-show() {
-  local f="$1"; shift
-
-  echo
-  echo "@@@@@@@@@@@@@@@@@@@ BEGIN $f"
-  sed 's/^/@@@/' "$f" || :
-  echo "@@@@@@@@@@@@@@@@@@@ END   $f"
-}
+export packageUrl="https://maven.pkg.github.com/$repoOwner/$repoName"
+###################################################
 
 if [[ ! -f "$file" ]]; then
   echo "PROBLEM: file not found: $file"
   exit 99
 fi
+if ! which mvn &>/dev/null; then
+  echo "PROBLEM: mvn not installed"
+  exit 99
+fi
+if ! which xmlstarlet &>/dev/null; then
+  echo "PROBLEM: xmlstarlet not installed"
+  exit 99
+fi
 
-cat > settings.xml <<EOF
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
-  <activeProfiles>
-    <activeProfile>github</activeProfile>
-  </activeProfiles>
+generateSettings() {
+  cat  <<EOF
+  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+    <activeProfiles>
+      <activeProfile>github</activeProfile>
+    </activeProfiles>
 
-  <profiles>
-    <profile>
-      <id>github</id>
-      <repositories>
-        <repository>
-          <id>central</id>
-          <url>https://repo1.maven.org/maven2</url>
-          <releases><enabled>true</enabled></releases>
-          <snapshots><enabled>false</enabled></snapshots>
-        </repository>
-        <repository>
-          <id>github</id>
-          <name>GitHub Apache Maven Packages</name>
-          <url>https://maven.pkg.github.com/$repoOwner/$repoName</url>
-        </repository>
-      </repositories>
-    </profile>
-  </profiles>
+    <profiles>
+      <profile>
+        <id>github</id>
+        <repositories>
+          <repository>
+            <id>central</id>
+            <url>https://repo1.maven.org/maven2</url>
+            <releases><enabled>true</enabled></releases>
+            <snapshots><enabled>false</enabled></snapshots>
+          </repository>
+          <repository>
+            <id>github</id>
+            <name>GitHub Apache Maven Packages</name>
+            <url>$packageUrl</url>
+          </repository>
+        </repositories>
+      </profile>
+    </profiles>
 
-  <servers>
-    <server>
-      <id>github</id>
-      <username>$tokenOwner</username>
-      <password>$token</password>
-    </server>
-  </servers>
-</settings>
+    <servers>
+      <server>
+        <id>github</id>
+        <username>$tokenOwner</username>
+        <password>$token</password>
+      </server>
+    </servers>
+  </settings>
 EOF
-
+}
+extractGaveFromPom() {
+  printf "%s:%s:%s:%s" \
+    "$(xmlstarlet sel -t -v /_:project/_:groupId    <pom.xml)" \
+    "$(xmlstarlet sel -t -v /_:project/_:artifactId <pom.xml)" \
+    "$(xmlstarlet sel -t -v /_:project/_:version    <pom.xml)" \
+    "$(xmlstarlet sel -t -v /_:project/_:packaging  <pom.xml)"
+}
 gave2vars() {
   local gave="$1"; shift
 
+  if [[ -f pom.xml && $gave == "" ]]; then
+    gave="$(extractGaveFromPom)"
+  fi
   export g a v e
   IFS=: read g a v e <<<"$gave"
 }
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+set
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
 gave2vars "$gave"
-set -x
 mvn \
   -X \
   -B \
-  -s settings.xml \
+  -s <(generateSettings) \
   deploy:deploy-file \
        -DgroupId="$g" \
     -DartifactId="$a" \
        -Dversion="$v" \
      -Dpackaging="$e" \
-  -DrepositoryId=github \
+  -DrepositoryId="github" \
           -Dfile="$file" \
-           -Durl="https://maven.pkg.github.com/$repoOwner/$repoName"
+           -Durl="$packageUrl"
