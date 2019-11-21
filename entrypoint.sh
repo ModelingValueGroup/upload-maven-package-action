28b3404d6e3ce1e738bd68d2b88807507fe777c3
Tom Brus <tombrus@gmail.com>
2019-11-22 00:04:07 +0100

    add check if version already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -103,7 +103,7 @@ query {
     repository(owner:"$USERNAME", name:"$REPOSNAME"){
         registryPackages(name:"$g.$a",first:1) {
             nodes {
-                versions(last:9999) {
+                versions(last:999) {
                     nodes {
                         version
                     }
