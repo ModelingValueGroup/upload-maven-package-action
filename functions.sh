b391854f41bd4667992f3fe8e1aca3bf329e609b
Tom Brus <tombrus@gmail.com>
2019-11-24 14:00:40 +0100

    make me depend on buildTools

--- functions.sh
+++ functions.sh
@@ -3,13 +3,15 @@
 ########################################################################################
 ########################################################################################
 main() (
-  echo "::group:: debugging" # TODO remove group, only for debug
   (
-    set -x
-    df -h
-    ls -la /mnt /mnt/* /run /run/*
-  ) | sed 's/^/@@@/' 1>&2
-  echo "::endgroup::"
+    echo "::group:: debugging" # TODO remove group, only for debug
+    (
+      set -x
+      df -h
+      ls -la /mnt /mnt/* /run /run/*
+    ) | sed 's/^/@@@ /'
+    echo "::endgroup::"
+  ) 1>&2
 
   local githubRepos="$1"; shift
   local       token="$1"; shift
