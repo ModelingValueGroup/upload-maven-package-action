67fdc5955bebce8458d6a926438a922fe17b47fc
Tom Brus <tombrus@gmail.com>
2019-11-24 15:45:54 +0100

    make me depend on buildTools

--- functions.sh
+++ functions.sh
@@ -9,7 +9,7 @@ main() (
   local        gave="$1"; shift
   local         pom="$1"; shift
 
-  includeBuildTools "$token" "1.0.2"
+  includeBuildTools "$token" "1.0.4"
 
   local githubPackageUrl="https://maven.pkg.github.com/$githubRepos"
 
