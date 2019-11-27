335b089ada756594c4385a812e44b6c64f6f8b3a
Tom Brus <tombrus@gmail.com>
2019-11-27 23:51:15 +0100

    try without docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,12 @@ if [[ "$INPUT_TRACE" == "true" ]]; then
 fi
 
 # this action repo exists on the client machine in:
-#    /home/runner/work/_actions/ModelingValueGroup/upload-maven-package-action
+#    /home/runner/work/_actions/ModelingValueGroup/upload-maven-package-action/<branch>
 
-. ../_actions/ModelingValueGroup/upload-maven-package-action/functions.sh
+ls -l ../_actions/ModelingValueGroup/upload-maven-package-action/*
+set
+
+. ../_actions/ModelingValueGroup/upload-maven-package-action/*/functions.sh
 
 main \
   "$INPUT_TOKEN" \
