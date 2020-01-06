5b93f90d60c05c4a37353dfdf783f7ab652b408b
Tom Brus <tombrus@gmail.com>
2020-01-06 22:59:55 +0100

    improve upload testing

--- functions.sh
+++ functions.sh
@@ -30,7 +30,7 @@ main() (
     local  gave="$1"; shift
     local   pom="$1"; shift
 
-    includeBuildTools "$token" "1.2.2"
+    includeBuildTools "$token" "1.2.3"
 
     ### check arguments
     if [[ ! -f "$file" ]]; then
